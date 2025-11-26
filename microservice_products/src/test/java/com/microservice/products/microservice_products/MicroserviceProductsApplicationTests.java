973b56de5690718ba9991a1e95e97887c8b039de
Riskoder <g.vidal1g+1@gmail.com>
2025-11-26 08:56:22 -0300

    Inicializacion de microservicios

---
+++ microservice_products/src/test/java/com/microservice/products/microservice_products/MicroserviceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.products.microservice_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
